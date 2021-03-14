bab3ed6255593439dc877c77bdcc11aff3d9a898
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-14 19:43:35 +0700

    Adding the remaining endpoints to the API class

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,6 +1,14 @@
 import 'package:covid_restapi_app/services/api_keys.dart';
 import 'package:flutter/material.dart';
 
+enum EndPoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class API {
   final String apiKey;
   API({@required this.apiKey});
@@ -10,4 +18,15 @@ class API {
   static final String host = 'ncov2019-admin.firebaseapp.com';
 
   Uri tokenUri() => Uri(scheme: 'https', host: host, path: 'token');
+
+  Uri endpointUri(EndPoint endpoint) =>
+      Uri(scheme: 'https', host: host, path: _paths[endpoint]);
+
+  static Map<EndPoint, String> _paths = {
+    EndPoint.cases: "case",
+    EndPoint.casesSuspected: "casesSuspected",
+    EndPoint.casesConfirmed: "casesConfirmed",
+    EndPoint.deaths: "deaths",
+    EndPoint.recovered: "recovered",
+  };
 }
