fc0f21a9f48d462f22dd0c12ea7b88e208a49548
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-14 19:56:40 +0700

    Requesting and parsing data from the remaining endpoints

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:covid_restapi_app/services/api.dart';
 
@@ -24,4 +25,36 @@ class APIServices {
       throw response;
     }
   }
+
+  Future<int> getEndpointData(
+      {@required String accessToken, @required EndPoint endpoint}) async {
+    final uri = api.endpointUri(endpoint);
+    final response =
+        await http.get(uri, headers: {'Authorization': 'Bearer $accessToken'});
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKeys = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKeys];
+        if (result != null) {
+          return result;
+        }
+      }
+
+      print(
+          'Request ${api.tokenUri().toString()} failed\nResponse: ${response.statusCode}, ${response.reasonPhrase}');
+      throw response;
+    }
+  }
+
+  static Map<EndPoint, String> _responseJsonKeys = {
+    EndPoint.cases: "case",
+    EndPoint.casesSuspected: "data",
+    EndPoint.casesConfirmed: "data",
+    EndPoint.deaths: "data",
+    EndPoint.recovered: "data",
+  };
 }
