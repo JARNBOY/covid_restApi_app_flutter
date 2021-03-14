1c3b41b728a41ee58e6c9c342a4f9f9093847f0f
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-14 21:15:01 +0700

    prepare UI dashboard screen

---
+++ lib/dashboard/dashboard_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'components/body_dashboard.dart';
+
+class DashBoardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BodyDashboard(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:covid_restapi_app/dashboard/dashboard_screen.dart';
 import 'package:covid_restapi_app/services/api.dart';
 import 'package:covid_restapi_app/services/api_service.dart';
 import 'package:flutter/material.dart';
@@ -11,20 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dashboard Covid 2019',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DashBoardScreen(), //MyHomePage(title: 'Flutter Demo Home Page')
     );
   }
 }
