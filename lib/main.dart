050f7bac9efd65885a449deead526b55bcf85456
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-14 19:30:15 +0700

    Using the API Service to show the access token

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:covid_restapi_app/services/api.dart';
+import 'package:covid_restapi_app/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -46,16 +48,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _accessToken = "";
 
-  void _incrementCounter() {
+  void _incrementCounter() async {
+    final apiService = APIServices(API.sanbox());
+    final accessToken = await apiService.getAccessToken();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _accessToken = accessToken;
     });
   }
 
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_accessToken',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
